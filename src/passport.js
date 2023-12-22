54162dcedd90db747a54a4301860c893031bf032
Ritika11294 <ritikagupta11294@gmail.com>
2023-12-23 02:27:33 +0530

    backend deployment

--- src/passport.js
+++ src/passport.js
@@ -18,7 +18,7 @@ const newToken = (user) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback",
+    callbackURL: "https://freecodecamp-deploy.onrender.com/auth/google/callback",
     passReqToCallback: true
 },
     async function (request, accessToken, refreshToken, profile, done) {
